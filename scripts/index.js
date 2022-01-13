81163742238daf65ddcd0ce9b0ce8f891c794ec7
Elena Dziuba <turin78952@mail.ru>
2022-01-13 19:53:00 +0300

    add styles and js

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+<SCRIPT LANGUAGE="JavaScript"></SCRIPT>
+let ButtonEditPopup = document.querySelector(selectors '.profile__edit-btn');
+let ButtonClosePopup = document.querySelector(selectors '.popup__close-btn');
+let Popup = document.querySelector(selectors '.popup')
+ButtonEditPopup.addEventListener('click', 'function')
